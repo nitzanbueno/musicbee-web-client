b1c4178b29429424a746e8b26bde19849a8c633c
Nitzan Bueno <nitzan.bueno@gmail.com>
2020-12-05 20:54:45 +0200

    Fix bug in search

--- src/Windows/SongPicker.tsx
+++ src/Windows/SongPicker.tsx
@@ -10,7 +10,7 @@ function doesTrackMatchQuery(track: Track, query?: string) {
     if (!query) return true;
 
     for (const fieldToSearch of TRACK_FIELDS_TO_SEARCH) {
-        if (track[fieldToSearch].toLocaleLowerCase().includes(query.toLocaleLowerCase())) return true;
+        if (track[fieldToSearch]?.toLocaleLowerCase()?.includes(query.toLocaleLowerCase())) return true;
     }
 
     return false;
