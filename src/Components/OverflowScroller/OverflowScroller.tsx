a306735512cbb1f4bf6ace1e9070f43855708bb7
Nitzan Bueno <nitzan.bueno@gmail.com>
2020-12-20 21:58:20 +0200

    Improve overflow scroll animation
    Make overflow scroll animation be an alternating seek,
    and have the animation be longer on longer text

--- src/Components/OverflowScroller/OverflowScroller.tsx
+++ src/Components/OverflowScroller/OverflowScroller.tsx
@@ -1,10 +1,15 @@
 import { useEffect, useRef, useState } from "react";
 import "./ScrollingText.css";
 
+// pixels per second
+const SPEED = 20;
+
 const OverflowScroller = props => {
     const bigScrollRef = useRef<HTMLDivElement>(null);
     const smallScrollRef = useRef<HTMLSpanElement>(null);
     const [scroll, setScroll] = useState(false);
+    const [textWidth, setTextWidth] = useState(0);
+    const [containerWidth, setContainerWidth] = useState(0);
 
     useEffect(() => {
         const big = bigScrollRef.current;
@@ -12,11 +17,22 @@ const OverflowScroller = props => {
 
         if (!big || !small) return;
         setScroll(big.scrollWidth > small.clientWidth);
+        setTextWidth(big.scrollWidth);
+        setContainerWidth(small.clientWidth);
     }, [props.children]);
+
+    const animationStyle: any = {
+        animationDuration: `${textWidth / SPEED}s`,
+        "--box-width": `${isNaN(containerWidth) ? 10 : containerWidth}px`,
+    };
 
     return (
         <span ref={smallScrollRef} className="no-wrap">
-            <div ref={bigScrollRef} className={scroll ? "scroll-left" : undefined}>
+            <div
+                ref={bigScrollRef}
+                style={scroll ? animationStyle : undefined}
+                className={scroll ? "scroll-left" : undefined}
+            >
                 {props.children}
             </div>
         </span>
